575ea271974942e3d3d9cff2b047476f11cb2f5a
Shashith Darshana <shashith.darshana@pearson.com>
2019-07-24 10:15:44 +0530

    Basic library setup and npm setup

---
+++ react-ebook-reader/src/component/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class EbookReader extends Component {
+    render() {
+        return <div>Test component for react ebook reader app</div>;
+    }
+}
+
+export default EbookReader;
